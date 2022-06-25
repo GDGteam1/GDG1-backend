564ae19facb335d7499640887372397b6fa1f27d
Taewan Kim <terry960302@gmail.com>
2022-06-26 04:17:01 +0900

    :sparkles: 재활용품 감지 기능 제작

--- src/main/java/com/example/gdg_project/vision/VisionController.java
+++ src/main/java/com/example/gdg_project/vision/VisionController.java
@@ -10,7 +10,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,35 +21,24 @@ public class VisionController {
     @PostMapping("/image-detect")
     public @ResponseBody String detectImage(@RequestParam("file") MultipartFile file) throws IOException {
         if( !file.isEmpty() ) {
-            System.out.printf("file org name = {}", file.getOriginalFilename());
-            System.out.printf("file content type = {}", file.getContentType());
-//            file.transferTo(new File(file.getOriginalFilename()));
-//
-//            File savedFile = new File("src/main/resources/images/" + file.getOriginalFilename());
 
             DetectImage di =  new DetectImage();
-            List<String> labels =  di.getLabels(file.getBytes());
+            List<String> labels =  di.getLabels(file.getBytes()).stream().map(s->s.toLowerCase(Locale.ROOT)).collect(Collectors.toList());
 
-            // 분기처리
-            // PET
-            if (labels.contains("Water") || labels.contains("Bottle")){
+            if ( labels.toString().contains("water") ||  labels.toString().contains("bottle")){
                 return "PET";
             }
-            if (labels.contains("can")){
+            if ( labels.toString().contains("can")){
                 return "can";
             }
-            if (labels.contains("Cup")){
+            if ( labels.toString().contains("cup")){
                 return "paper";
             }
 
-            return "success";
-
-
+            return "재활용이 불가능합니다. 일반쓰레기에 버려주십시오.";
         }
 
-
-
-        return "fail";
+        return "no such file on server";
     }
 
 }
